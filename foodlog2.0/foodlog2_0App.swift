926a9895a60526d79bcc92f868bda80fa8580483
spicyspuds <miguel.pereira.canada@gmail.com>
2025-03-30 15:12:08 -0400

    Initial Commit

---
+++ foodlog2.0/foodlog2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  foodlog2_0App.swift
+//  foodlog2.0
+//
+//  Created by Miguel Pereira on 2025-03-30.
+//
+
+import SwiftUI
+
+@main
+struct foodlog2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
